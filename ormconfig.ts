b425a77bd19a36085ac59133ec165939f6e90ec1
“phuoctrancong” <tcphuoc.dev.humg@gmail.com>
2023-10-19 23:21:07 +0700

    fix  ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,30 @@
 const SnakeNamingStrategy =
   require('typeorm-naming-strategies').SnakeNamingStrategy;
 
+// module.exports = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'P@ssword',
+//   database: 'wmsx_sell_lock',
+//   entities: ['dist/src/entities/*.entity.js'],
+//   migrations: ['dist/src/migrations/*.js'],
+//   synchronize: false,
+//   cli: {
+//     entitiesDir: 'src/entities',
+//     migrationsDir: 'src/migrations',
+//   },
+//   logging: true,
+//   namingStrategy: new SnakeNamingStrategy(),
+// };
 module.exports = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: 'P@ssword',
-  database: 'wmsx_sell_lock',
+  password: 'P@ssword', //Em thay password cảu em vào đây nhé
+  database: 'sell_locks', //Tạo thêm cho anh 1 cái databasse như này nhé
   entities: ['dist/src/entities/*.entity.js'],
   migrations: ['dist/src/migrations/*.js'],
   synchronize: false,
